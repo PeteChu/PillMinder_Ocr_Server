5826d42ca0eadc988c4cda946085d26855b32e18
schecterza <phuengton.ch.57@ubu.ac.th>
2018-03-07 22:24:15 +0700

    change get_close_matches accuracy to 0.8

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ from difflib import get_close_matches
 
 
 def findMedicineName(medicine_list, text_list):
-
     for i in text_list:
-        close_match = get_close_matches(i, medicine_list)
+        close_match = get_close_matches(i, medicine_list, cutoff=0.8)
         if close_match:
             return close_match[0]
     return -1
@@ -18,7 +17,7 @@ if __name__ == '__main__':
     medicine_list = ["AMLODIPINE"]
 
     image_path = './images/image1.jpg'
-    
+
     cropped_image = crop(image_path)
 
     text_list = imageToText(cropped_image)
