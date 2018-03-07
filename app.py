62af52922c5bba9f7a4a28db5ac22f84d2aa88da
schecterza <phuengton.ch.57@ubu.ac.th>
2018-03-07 20:20:25 +0700

    add ocr & findmedname function

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 import cv2
 from crop_image import crop
-from ocr import image_to_text
+from ocr import imageToText
+from difflib import get_close_matches
+
+def findMedicineName(medicine_list, text):
+
+    for i in range(len(medicine_list)):
+        word = medicine_list[i]
+        length = len(word)
+        for j in range(len(text)):
+            if j+length < len(text):
+                tmp = text[j: j+length]
+                close_match = get_close_matches(tmp, medicine_list, cutoff=0.8)
+                if close_match:
+                    return close_match[0]
+    return -1
 
 if __name__ == '__main__':
+
+    medicine_list = ["AMLODIPINE"]
 
     image_path = './images/image1.jpg'
     image = cv2.imread(image_path)
 
     cropped_image = crop(image)
 
-    text = image_to_text(cropped_image)
+    text = imageToText(cropped_image)
 
-    print(text)
+    medicine_name = findMedicineName(medicine_list, text)
 
+    print(medicine_name)
--- ocr.py
+++ ocr.py
@@ -2,30 +2,30 @@ from PIL import Image
 import cv2
 import pytesseract
 import os
+import re
 
 
-def cvt_to_bw(image):
+def cvtToBw(image):
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    blur = cv2.medianBlur(gray, 3)
     rect, bw_image = cv2.threshold(
-        gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    blur = cv2.GaussianBlur(bw_image, (0, 0), 3)
+        blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
     filename = './images/ocr/{}.jpg'.format(os.getpid())
-    cv2.imwrite(filename, blur)
+    cv2.imwrite(filename, bw_image)
     return filename
 
 
-def image_to_text(image_path):
+def imageToText(image_path):
 
     tessdata_dir_config = '--tessdata-dir "./langdata"'
 
-    bw_image_path = cvt_to_bw(image_path)
+    bw_image_path = cvtToBw(image_path)
 
     text = pytesseract.image_to_string(Image.open(
         bw_image_path), lang='tha+eng', config=tessdata_dir_config)
     os.remove(bw_image_path)
-
-    return text
+    pattern = re.compile('[^a-zA-Z]')
+    return pattern.sub('', text)
