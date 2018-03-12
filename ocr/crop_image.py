bc9af9e67ff75aa0a396ed35d1cb74ba8a284f78
Phuengton Chummuel <phuengton.ch.57@ubu.ac.th>
2018-03-12 21:25:17 +0700

    add ocr folder

---
+++ ocr/crop_image.py
@@ -0,0 +1,31 @@
+import cv2
+import os
+
+from numba import jit
+
+@jit
+def crop(image_path):
+
+    image = cv2.imread(image_path)
+    resize = cv2.resize(image, (2000, 1000))
+    original_image = resize
+    gray = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    rect, threshold = cv2.threshold(
+        blur, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    im2, contours, hierarchy = cv2.findContours(
+        threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    if len(contours) != 0:
+
+        # draw contours
+        cv2.drawContours(original_image, contours, -1, 255, 3)
+
+        # find largest contour
+        c = max(contours, key=cv2.contourArea)
+
+        x,y,w,h = cv2.boundingRect(c)
+
+    return original_image[y: y+h, x:x+w]
---
+++ ocr/find_medicine_name.py
@@ -0,0 +1,19 @@
+from difflib import get_close_matches
+import json
+import os
+
+
+def findMedicineName(text_list):
+
+    close_match = []
+
+    with open('medicine_list.json', encoding='utf-8') as json_data:
+        medicine_list = json.load(json_data)['medicineList']
+
+    for i in text_list:
+        close_matches = get_close_matches(i, medicine_list, cutoff=0.8)
+        if close_matches:
+            for i in close_matches:
+                close_match.append(i)
+            
+    return list(set(close_match)) if close_match else -1
